400bcec902064ca14831b26f4565ab53a4a63ba1
John Blum <jblum@pivotal.io>
2018-05-28 22:46:22 -0700

    Add temporary GemfireBeanFactoryLocatorProxy class extending SDG's GemfireBeanFactoryLocator to clean up references to BeanFactories stored by integration tests.

---
+++ geode-spring-boot-starter/src/test/java/org/springframework/data/gemfire/support/GemfireBeanFactoryLocatorProxy.java
@@ -0,0 +1,11 @@
+package org.springframework.data.gemfire.support;
+
+/**
+ * The GemfireBeanFactoryLocatorProxy class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+public class GemfireBeanFactoryLocatorProxy {
+
+}
