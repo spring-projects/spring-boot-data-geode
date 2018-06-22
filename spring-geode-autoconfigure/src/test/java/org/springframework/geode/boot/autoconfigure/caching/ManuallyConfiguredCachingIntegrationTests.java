cd5399a3c3bf73b2dc260d151dcff1b4059046be
John Blum <jblum@pivotal.io>
2018-06-22 01:19:35 -0700

    Add integration test asserting neither Apache Geode nor Pivotal GemFire is configured as the cache provider in Spring's Cache Abstraction when another caching provider (i.e. CacheManger) has been declared and configured in the Spring context.

---
+++ spring-geode-autoconfigure/src/test/java/org/springframework/geode/boot/autoconfigure/caching/ManuallyConfiguredCachingIntegrationTests.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.springframework.geode.boot.autoconfigure.caching;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.cache.GemfireCacheManager;
+import org.springframework.data.gemfire.tests.integration.IntegrationTestsSupport;
+import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
+import org.springframework.geode.boot.autoconfigure.CachingProviderAutoConfiguration;
+import org.springframework.geode.boot.autoconfigure.ContinuousQueryAutoConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Integration tests for {@link CachingProviderAutoConfiguration} asserting that neither Apache Geode
+ * nor Pivotal GemFire is configured as the caching provider in Spring's Cache Abstraction when another
+ * caching provider (i.e. {@link CacheManager} bean) has been declared and configured.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.test.context.SpringBootTest
+ * @see org.springframework.cache.CacheManager
+ * @see org.springframework.context.ApplicationContext
+ * @see org.springframework.data.gemfire.cache.GemfireCacheManager
+ * @see org.springframework.data.gemfire.tests.integration.IntegrationTestsSupport
+ * @see org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects
+ * @see org.springframework.geode.boot.autoconfigure.CachingProviderAutoConfiguration
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @since 1.0.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SuppressWarnings("unused")
+public class ManuallyConfiguredCachingIntegrationTests extends IntegrationTestsSupport {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void gemfireCacheManagerNotPresent() {
+
+		assertThat(this.applicationContext).isNotNull();
+		assertThat(this.applicationContext.containsBean("gemfireCache"));
+		assertThat(this.applicationContext.containsBean("cacheManager"));
+
+		CacheManager mockCacheManager = this.applicationContext.getBean("cacheManager", CacheManager.class);
+
+		assertThat(mockCacheManager).isNotNull();
+		assertThat(mockCacheManager).isNotInstanceOf(GemfireCacheManager.class);
+	}
+
+	@EnableGemFireMockObjects
+	@SpringBootApplication(exclude = ContinuousQueryAutoConfiguration.class)
+	static class TestConfiguration {
+
+		@Bean
+		CacheManager cacheManager() {
+			return mock(CacheManager.class);
+		}
+	}
+}
