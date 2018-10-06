43245b68aabf70d813cd80c531a4e040929e07f0
John Blum <jblum@pivotal.io>
2018-10-05 18:36:28 -0700

    Add integration tests asserting that Spring Session for Apache Geode/Pivotal GemFire is not auto-configured when the Spring ApplicationContext is not a WebApplicationContext.

---
+++ spring-geode-autoconfigure/src/test/java/org/springframework/geode/boot/autoconfigure/session/NoAutoConfigurationOfSessionCachingIntegrationTests.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.springframework.geode.boot.autoconfigure.session;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
+import org.springframework.geode.boot.autoconfigure.ContinuousQueryAutoConfiguration;
+import org.springframework.geode.boot.autoconfigure.SpringSessionAutoConfiguration;
+import org.springframework.session.SessionRepository;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * Integration tests for {@link SpringSessionAutoConfiguration} asserting that Spring Session
+ * (for Apache Geode/Pivotal GemFire) is not auto-configured when the Spring {@link ApplicationContext}
+ * is not a {@link WebApplicationContext}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.test.context.SpringBootTest
+ * @see org.springframework.context.ApplicationContext
+ * @see org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects
+ * @see org.springframework.geode.boot.autoconfigure.SpringSessionAutoConfiguration
+ * @see org.springframework.session.SessionRepository
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @see org.springframework.web.context.WebApplicationContext
+ * @since 1.0.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SuppressWarnings("unused")
+public class NoAutoConfigurationOfSessionCachingIntegrationTests {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	public void noSessionRepositoryBeanIsPresent() {
+
+		assertThat(this.applicationContext).isNotNull();
+		assertThat(this.applicationContext.containsBean("sessionRepository")).isFalse();
+		assertThat(this.applicationContext.getBeansOfType(SessionRepository.class)).isEmpty();
+	}
+
+	@EnableGemFireMockObjects
+	@SpringBootApplication(exclude = ContinuousQueryAutoConfiguration.class)
+	static class TestConfiguration { }
+
+}
