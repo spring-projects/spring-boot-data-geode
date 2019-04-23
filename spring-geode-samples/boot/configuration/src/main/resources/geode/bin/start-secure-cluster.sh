58d9970ce1182606aecad3f32c4c7d54acc40b8c
John Blum <jblum@pivotal.io>
2019-04-22 23:44:40 -0700

    Edit guide and refactor example of Spring Boot auto-configuration for Apache Geode/Pivotal GemFire to include Security with Authentication.

---
+++ spring-geode-samples/boot/configuration/src/main/resources/geode/bin/start-secure-cluster.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+gfsh -e "run --file=${SBDG_HOME}/spring-geode-samples/boot/configuration/src/main/resources/geode/bin/start-secure-cluster.gfsh"
+#gfsh -e "run --file=/Users/jblum/pivdev/spring-boot-data-geode/spring-geode-samples/boot/configuration/src/main/resources/geode/bin/start-secure-cluster.gfsh"
