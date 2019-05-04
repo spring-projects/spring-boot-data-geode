4f7383f71b0d8c918289a7a82d6108b67d77a68b
John Blum <jblum@pivotal.io>
2019-05-03 19:10:13 -0700

    Encapsulate configuration of Apache Geode/Pivotal GemFire (PCC) Security (Auth) using an enabling Annotation and security-manager property with proxying.

---
+++ spring-geode/src/main/java/org/springframework/geode/config/annotation/SecurityManagerProxyConfiguration.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.springframework.geode.config.annotation;
+
+import java.util.Properties;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.data.gemfire.config.annotation.EnableBeanFactoryLocator;
+import org.springframework.data.gemfire.config.annotation.EnableSecurity;
+import org.springframework.geode.security.support.SecurityManagerProxy;
+
+/**
+ * Spring {@link Configuration} class used to configure a {@link org.apache.geode.security.SecurityManager},
+ * thereby enabling Security (Auth) on this GemFire/Geode node.
+ *
+ * @author John Blum
+ * @see org.springframework.context.ApplicationListener
+ * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.context.event.ContextRefreshedEvent
+ * @see org.springframework.data.gemfire.config.annotation.EnableBeanFactoryLocator
+ * @see org.springframework.data.gemfire.config.annotation.EnableSecurity
+ * @see org.springframework.geode.security.support.SecurityManagerProxy
+ * @since 1.1.0
+ */
+@Configuration
+@EnableBeanFactoryLocator
+@EnableSecurity(securityManagerClassName = "org.springframework.geode.security.support.SecurityManagerProxy")
+@SuppressWarnings("unused")
+public class SecurityManagerProxyConfiguration implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+
+		SecurityManagerProxy securityManagerProxy = SecurityManagerProxy.getInstance();
+
+		securityManagerProxy.setBeanFactory(event.getApplicationContext().getAutowireCapableBeanFactory());
+		securityManagerProxy.init(new Properties());
+	}
+}
