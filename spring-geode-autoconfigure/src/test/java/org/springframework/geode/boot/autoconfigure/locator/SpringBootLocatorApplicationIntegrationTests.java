9ed041395640eec3e2d3e055be3c92f20837e26c
John Blum <jblum@pivotal.io>
2019-05-15 15:41:21 -0700

    Add support for Spring configured and bootstrapped Locator-based applications.
    Resolves gh-37.

---
+++ spring-geode-autoconfigure/src/test/java/org/springframework/geode/boot/autoconfigure/locator/SpringBootLocatorApplicationIntegrationTests.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.springframework.geode.boot.autoconfigure.locator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+import org.apache.geode.cache.GemFireCache;
+import org.apache.geode.cache.client.ClientCache;
+import org.apache.geode.distributed.Locator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
+import org.springframework.geode.boot.autoconfigure.ContinuousQueryAutoConfiguration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Integration Tests asserting that a {@link ClientCache} is not auto-configured by SBDG if a {{@link Locator} bean
+ * is present in the Spring container.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.apache.geode.cache.GemFireCache
+ * @see org.apache.geode.cache.client.ClientCache
+ * @see org.apache.geode.distributed.Locator
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects
+ * @see org.springframework.test.context.ContextConfiguration
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @since 1.1.0
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@SuppressWarnings("unused")
+public class SpringBootLocatorApplicationIntegrationTests {
+
+	@Autowired(required = false)
+	private GemFireCache clientCache;
+
+	@Autowired
+	private Locator mockLocator;
+
+	@Test
+	public void noCacheInstanceIsAutoConfiguredWhenLocatorBeanIsPresent() {
+
+		assertThat(this.clientCache).isNull();
+		assertThat(this.mockLocator).isNotNull();
+	}
+
+	@EnableGemFireMockObjects
+	@SpringBootApplication(exclude = ContinuousQueryAutoConfiguration.class)
+	static class TestConfiguration {
+
+		@Bean
+		Locator mockLocator() {
+			return mock(Locator.class);
+		}
+	}
+}
