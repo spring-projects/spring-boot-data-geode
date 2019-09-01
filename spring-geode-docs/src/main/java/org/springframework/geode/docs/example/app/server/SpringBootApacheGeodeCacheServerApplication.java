d0fe5800d807908fc6022c62faf3bdcbb31faba4
John Blum <jblum@pivotal.io>
2019-09-01 10:55:38 -0700

    Format source code.

--- spring-geode-docs/src/main/java/org/springframework/geode/docs/example/app/server/SpringBootApacheGeodeCacheServerApplication.java
+++ spring-geode-docs/src/main/java/org/springframework/geode/docs/example/app/server/SpringBootApacheGeodeCacheServerApplication.java
@@ -13,7 +13,6 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 package org.springframework.geode.docs.example.app.server;
 
 import org.springframework.boot.WebApplicationType;
