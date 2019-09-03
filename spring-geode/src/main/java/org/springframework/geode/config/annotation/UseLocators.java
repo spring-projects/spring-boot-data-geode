2fc6a28de29b88541c3dee8e92c743deac619373
John Blum <jblum@pivotal.io>
2019-09-03 14:39:52 -0700

    Format source code; Optimize imports.

--- spring-geode/src/main/java/org/springframework/geode/config/annotation/UseLocators.java
+++ spring-geode/src/main/java/org/springframework/geode/config/annotation/UseLocators.java
@@ -13,7 +13,6 @@
  * or implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
-
 package org.springframework.geode.config.annotation;
 
 import static org.springframework.geode.config.annotation.LocatorsConfiguration.DEFAULT_LOCATORS;
@@ -27,6 +26,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.apache.geode.cache.Cache;
+
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
 
