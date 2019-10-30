893989d81b3b86e078747463b9c1cf360a6dce32
John Blum <jblum@pivotal.io>
2019-10-30 12:26:26 -0700

    Add additional Unit Test for the auto-configuration of Apache Geode as a caching provider in Spring's Cache Abstraction using Mock Apache Geode Objects.

---
+++ spring-geode-autoconfigure/src/test/java/example/test/service/TestCacheableService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package example.test.service;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+/**
+ * {@link TestCacheableService} is a Spring {@link Service} bean with caching applied.
+ *
+ * @author John Blum
+ * @see java.util.Random
+ * @see org.springframework.cache.annotation.Cacheable
+ * @see org.springframework.stereotype.Service
+ * @since 1.2.1
+ */
+@Service
+public class TestCacheableService {
+
+	private final AtomicBoolean cacheMiss = new AtomicBoolean(false);
+
+	private final Random random = new Random(System.currentTimeMillis());
+
+	public boolean isCacheMiss() {
+		return this.cacheMiss.getAndSet(false);
+	}
+
+	@Cacheable("RandomNumbers")
+	public Number getRandomNumber(@SuppressWarnings("unused") String key) {
+
+		this.cacheMiss.set(true);
+
+		return this.random.nextInt();
+	}
+}
---
+++ spring-geode-autoconfigure/src/test/java/org/springframework/geode/boot/autoconfigure/caching/AutoConfiguredCachingUnitTests.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.springframework.geode.boot.autoconfigure.caching;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
+import org.springframework.data.gemfire.tests.integration.IntegrationTestsSupport;
+import org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects;
+import org.springframework.geode.boot.autoconfigure.CachingProviderAutoConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import example.test.service.TestCacheableService;
+
+/**
+ * Unit Tests for {@link CachingProviderAutoConfiguration} using Mock Apache Geode {@link Object Objects}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.test.context.SpringBootTest
+ * @see org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions
+ * @see org.springframework.data.gemfire.tests.mock.annotation.EnableGemFireMockObjects
+ * @see org.springframework.data.gemfire.tests.integration.IntegrationTestsSupport
+ * @see org.springframework.geode.boot.autoconfigure.CachingProviderAutoConfiguration
+ * @see org.springframework.test.context.junit4.SpringRunner
+ * @since 1.2.1
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SuppressWarnings("unused")
+public class AutoConfiguredCachingUnitTests extends IntegrationTestsSupport {
+
+	@Autowired
+	private TestCacheableService cacheableService;
+
+	@Before
+	public void setup() {
+		assertThat(this.cacheableService).isNotNull();
+		assertThat(this.cacheableService.isCacheMiss()).isFalse();
+	}
+
+	@Test
+	public void cachingIsConfiguredAndWorkingCorrectly() {
+
+		Number randomNumber = this.cacheableService.getRandomNumber("A");
+
+		assertThat(randomNumber).isNotNull();
+		assertThat(this.cacheableService.isCacheMiss()).isTrue();
+
+		Number sameRandomNumber = this.cacheableService.getRandomNumber("A");
+
+		assertThat(sameRandomNumber).isEqualTo(randomNumber);
+		assertThat(this.cacheableService.isCacheMiss()).isFalse();
+
+		Number anotherRandomNumber = this.cacheableService.getRandomNumber("B");
+
+		assertThat(anotherRandomNumber).isNotNull();
+		assertThat(anotherRandomNumber).isNotEqualTo(randomNumber);
+		assertThat(this.cacheableService.isCacheMiss()).isTrue();
+	}
+
+	@SpringBootConfiguration
+	@EnableAutoConfiguration
+	@EnableCachingDefinedRegions
+	@EnableGemFireMockObjects
+	static class TestConfiguration {
+
+		@Bean
+		TestCacheableService testCacheableService() {
+			return new TestCacheableService();
+		}
+	}
+}
