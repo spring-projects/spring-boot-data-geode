796f15e87688078ff9f94be2146a65206f35c750
John Blum <jblum@pivotal.io>
2020-02-21 19:29:36 -0800

    Change the GeodeConstants abstract class to an interface.

--- apache-geode-extensions/src/main/java/org/springframework/geode/util/GeodeConstants.java
+++ apache-geode-extensions/src/main/java/org/springframework/geode/util/GeodeConstants.java
@@ -19,23 +19,23 @@ import org.apache.geode.distributed.internal.DistributionConfig;
 import org.apache.geode.management.internal.security.ResourceConstants;
 
 /**
- * Abstract utility class encapsulating common Apache Geode constants used by SBDG.
+ * Interface encapsulating common Apache Geode constants used by SBDG.
  *
  * @author John Blum
  * @since 1.3.0
  */
-public abstract class GeodeConstants {
+public interface GeodeConstants {
 
-	public static final String GEMFIRE_PROPERTY_PREFIX = DistributionConfig.GEMFIRE_PREFIX;
+	String GEMFIRE_PROPERTY_PREFIX = DistributionConfig.GEMFIRE_PREFIX;
 
 	// Logging Constants (referring to Properties)
-	public static final String LOG_DISK_SPACE_LIMIT = DistributionConfig.LOG_DISK_SPACE_LIMIT_NAME;
-	public static final String LOG_FILE = DistributionConfig.LOG_FILE_NAME;
-	public static final String LOG_FILE_SIZE_LIMIT = DistributionConfig.LOG_FILE_SIZE_LIMIT_NAME;
-	public static final String LOG_LEVEL = DistributionConfig.LOG_LEVEL_NAME;
+	String LOG_DISK_SPACE_LIMIT = DistributionConfig.LOG_DISK_SPACE_LIMIT_NAME;
+	String LOG_FILE = DistributionConfig.LOG_FILE_NAME;
+	String LOG_FILE_SIZE_LIMIT = DistributionConfig.LOG_FILE_SIZE_LIMIT_NAME;
+	String LOG_LEVEL = DistributionConfig.LOG_LEVEL_NAME;
 
 	// Security Constants (referring to Properties)
-	public static final String PASSWORD = ResourceConstants.PASSWORD;
-	public static final String USERNAME = ResourceConstants.USER_NAME;
+	String PASSWORD = ResourceConstants.PASSWORD;
+	String USERNAME = ResourceConstants.USER_NAME;
 
 }
