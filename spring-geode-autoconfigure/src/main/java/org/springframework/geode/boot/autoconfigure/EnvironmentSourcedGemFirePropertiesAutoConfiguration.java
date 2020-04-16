39d81d339a42fddd178005334ad09aae6948fc66
John Blum <jblum@pivotal.io>
2020-04-15 22:14:21 -0700

    Remove conditional on missing GemFireCache bean annotation declaration.
    This is necessary to apply Geode Properties declared in Spring Boot application.properties to Spring Boot, Apache Geode, Peer Cache applications.
    
    Resolves gh-79.

--- spring-geode-autoconfigure/src/main/java/org/springframework/geode/boot/autoconfigure/EnvironmentSourcedGemFirePropertiesAutoConfiguration.java
+++ spring-geode-autoconfigure/src/main/java/org/springframework/geode/boot/autoconfigure/EnvironmentSourcedGemFirePropertiesAutoConfiguration.java
@@ -25,7 +25,6 @@ import org.apache.geode.cache.GemFireCache;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -47,7 +46,7 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Spring Boot {@link EnableAutoConfiguration auto-configuration} enabling the processing of
- * {@literal gemfire.properties} declared in Spring Boot {@literal application.properties}.
+ * {@literal gemfire.properties}, or Geode {@link Properties} declared in Spring Boot {@literal application.properties}.
  *
  * @author John Blum
  * @see java.util.Properties
@@ -55,6 +54,8 @@ import org.slf4j.LoggerFactory;
  * @see org.springframework.boot.autoconfigure.EnableAutoConfiguration
  * @see org.springframework.context.annotation.Bean
  * @see org.springframework.context.annotation.Configuration
+ * @see org.springframework.core.Ordered
+ * @see org.springframework.core.annotation.Order
  * @see org.springframework.core.env.ConfigurableEnvironment
  * @see org.springframework.core.env.EnumerablePropertySource
  * @see org.springframework.core.env.MutablePropertySources
@@ -62,12 +63,12 @@ import org.slf4j.LoggerFactory;
  * @see org.springframework.data.gemfire.GemFireProperties
  * @see org.springframework.data.gemfire.config.annotation.ClientCacheConfigurer
  * @see org.springframework.data.gemfire.config.annotation.PeerCacheConfigurer
+ * @see <a href="https://geode.apache.org/docs/guide/112/reference/topics/gemfire_properties.html">Geode Properties</a>
  * @since 1.3.0
  */
 @Configuration
 @ConditionalOnClass({ GemFireCache.class, CacheFactoryBean.class })
 @AutoConfigureBefore({ ClientCacheAutoConfiguration.class })
-@ConditionalOnMissingBean(GemFireCache.class)
 @SuppressWarnings("unused")
 public class EnvironmentSourcedGemFirePropertiesAutoConfiguration {
 
