8ed0f24d8baaf1515c24a9e520e8de7c2ad5cdcf
John Blum <jblum@pivotal.io>
2020-07-07 11:20:15 -0700

    Add ByteArrayResourceReader implementation that reads the data from a Resource (e.g. ClassPathResource) into a byte array.
    Resolves gh-92.

---
+++ spring-geode/src/main/java/org/springframework/geode/core/io/support/ByteArrayResourceReader.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.springframework.geode.core.io.support;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.core.io.Resource;
+import org.springframework.geode.core.io.AbstractResourceReader;
+import org.springframework.lang.NonNull;
+
+/**
+ * A concrete {@link AbstractResourceReader} implementation that reads data from a target {@link Resource Resource's}
+ * {@link Resource#getInputStream() InputStream} into a byte array.
+ *
+ * @author John Blum
+ * @see java.io.InputStream
+ * @see java.io.ByteArrayOutputStream
+ * @see org.springframework.core.io.Resource
+ * @see org.springframework.geode.core.io.AbstractResourceReader
+ * @since 1.3.1
+ */
+@SuppressWarnings("unused")
+public class ByteArrayResourceReader extends AbstractResourceReader {
+
+	protected static final int DEFAULT_BUFFER_SIZE = 32768;
+
+	/**
+	 * Returns the required {@link Integer#TYPE buffer size} used to capture data from the target {@link Resource}
+	 * in chunks.
+	 *
+	 * Subclasses are encouraged to override this method as necessary to tune the buffer size.  By default, the
+	 * buffer size is {@literal 32K} or {@literal 32768} bytes.
+	 *
+	 * @return the required {@link Integer#TYPE buffer size} to read from the {@link Resource} in chunks.
+	 */
+	protected int getBufferSize() {
+		return DEFAULT_BUFFER_SIZE;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	protected @NonNull byte[] doRead(@NonNull InputStream resourceInputStream) throws IOException {
+
+		try (ByteArrayOutputStream out = new ByteArrayOutputStream(resourceInputStream.available())) {
+
+			byte[] buffer = new byte[getBufferSize()];
+
+			for (int bytesRead = resourceInputStream.read(buffer); bytesRead != -1; bytesRead = resourceInputStream.read(buffer)) {
+				// using a buffer will trigger a flush() automatically in the OutputStream
+				out.write(buffer, 0, bytesRead);
+			}
+
+			out.flush();
+
+			return out.toByteArray();
+		}
+	}
+}
---
+++ spring-geode/src/test/java/org/springframework/geode/core/io/support/ByteArrayResourceReaderUnitTests.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.springframework.geode.core.io.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import org.springframework.core.io.Resource;
+
+/**
+ * Unit Tests for {@link ByteArrayResourceReader}.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.springframework.core.io.Resource
+ * @see org.springframework.geode.core.io.support.ByteArrayResourceReader
+ * @since 1.3.1
+ */
+public class ByteArrayResourceReaderUnitTests {
+
+	@Test
+	public void doReadResourceReturnsByteArray() throws IOException {
+
+		byte[] data = { (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE };
+
+		ByteArrayInputStream in = spy(new ByteArrayInputStream(data));
+
+		ByteArrayResourceReader reader = spy(new ByteArrayResourceReader());
+
+		Resource mockResource = mock(Resource.class);
+
+		doReturn(in).when(mockResource).getInputStream();
+		doReturn(2).when(reader).getBufferSize();
+
+		assertThat(reader.read(mockResource)).isEqualTo(data);
+
+		verify(mockResource, times(1)).getInputStream();
+		verify(in, times(1)).available();
+		verify(in, times(3)).read(isA(byte[].class));
+		verify(reader, times(1)).doRead(eq(in));
+	}
+
+	@Test
+	public void getBufferSizeIsDefault() {
+		assertThat(new ByteArrayResourceReader().getBufferSize())
+			.isEqualTo(ByteArrayResourceReader.DEFAULT_BUFFER_SIZE);
+	}
+}
