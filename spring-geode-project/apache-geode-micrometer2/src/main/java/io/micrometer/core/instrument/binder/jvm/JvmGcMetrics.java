f5ab7e537c72e2a5ab97de67fed154254410217e
John Blum <jblum@vmware.com>
2022-04-06 13:11:20 -0700

    Add new apache-geode-micrometer2 module.
    The apache-geode-micrometer2 module includes integration support for Apache Geode 1.x with Micrometer 2.x.

---
+++ spring-geode-project/apache-geode-micrometer2/src/main/java/io/micrometer/core/instrument/binder/jvm/JvmGcMetrics.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package io.micrometer.core.instrument.binder.jvm;
+
+import io.micrometer.core.instrument.Tag;
+
+/**
+ * Extension of {@link io.micrometer.binder.jvm.JvmGcMetrics} for adapting and repackaging purposes
+ * as required by Apache Geode 1.x, which is based on Micrometer 1.x.
+ *
+ * @author John Blum
+ * @see io.micrometer.binder.jvm.JvmGcMetrics
+ * @since 2.0.0
+ */
+@SuppressWarnings("unused")
+public class JvmGcMetrics extends io.micrometer.binder.jvm.JvmGcMetrics {
+
+	/**
+	 * @inheritDoc
+	 */
+	public JvmGcMetrics() { }
+
+	/**
+	 * @inheritDoc
+	 */
+	public JvmGcMetrics(Iterable<Tag> tags) {
+		super(tags);
+	}
+}
