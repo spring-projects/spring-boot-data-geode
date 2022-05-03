9a17e8f128635f1c68d5c0c9a6c7a6a636b25407
John Blum <jblum@vmware.com>
2022-05-03 15:45:03 -0700

    Disable Docker login.
    Explicit Docker login is not required when 1) using the VMW Harbor DockerHub proxy and 2) the project build does not use Testcontainers.

--- ci/setup.sh
+++ ci/setup.sh
@@ -6,6 +6,6 @@
 
 echo "Logged into Jenkins CI as user [$USER] with home directory [$HOME] in the current working directory [$PWD]"
 chown -R 1001:1001 .
-echo "Logging into Docker..."
-docker login --username ${DOCKER_HUB_USR} --password ${DOCKER_HUB_PSW}
+#echo "Logging into Docker..."
+#docker login --username ${DOCKER_HUB_USR} --password ${DOCKER_HUB_PSW}
 exit 0
