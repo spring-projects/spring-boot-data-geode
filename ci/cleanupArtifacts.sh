75b1b3bcc068a48a3449a8a16957d3a2c9f61f8e
John Blum <jblum@vmware.com>
2023-05-12 13:02:51 -0700

    Refactor the ci/cleanupArtifacts.sh script to disable the removal of .gradle/ and .m2/ directory (cached) files.

--- ci/cleanupArtifacts.sh
+++ ci/cleanupArtifacts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-rm -Rf ./.gradle
-rm -Rf ./.m2
+#rm -Rf ./.gradle
+#rm -Rf ./.m2
 rm -Rf `find . -name "build" | grep -v "src"`
 rm -Rf `find . -name "target" | grep -v "src"`
 exit 0
